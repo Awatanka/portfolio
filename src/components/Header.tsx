b167209a4586a877935d8a7e829b2394dda18592
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-06-07 13:17:58 -0700

    Refactored code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -51,7 +51,6 @@ export default function DrawerAppBar({ window }: Props) {
       const element = document.getElementById(id);
       if (element) {
         element.scrollIntoView({ behavior: "smooth" });
-        handleDrawerToggle();
       }
     }
   };
--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -107,86 +107,88 @@ const ProjectSection = () => {
 
   return (
     <>
-      <Box sx={{ display: "flex", flexDirection: "column" }} id="projects">
-        <InView>
-          <motion.div
-            ref={ref}
-            initial={{ opacity: 0, y: 50 }}
-            animate={inView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 1 }}
-          >
-            <Typography
-              variant="h4"
-              style={{
-                padding: "40px",
-                borderLeft: "3px solid #A5B3C7",
-                margin: "20px 0",
-                color: "#013B41",
-              }}
+      <Box sx={{ margineTop: "60px" }} id="projects">
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <InView>
+            <motion.div
+              ref={ref}
+              initial={{ opacity: 0, y: 50 }}
+              animate={inView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 1 }}
             >
-              Welcome to my projects section!
-            </Typography>
-          </motion.div>
-          <motion.div
-            ref={ref}
-            initial={{ opacity: 0, y: 50 }}
-            animate={inView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 1 }}
-          >
-            <Typography
-              style={{
-                padding: "20px 0",
-                margin: "20px 0",
-                color: "#013B41",
-                fontSize: "18px",
-                lineHeight: "1.6",
-                textAlign: "justify",
-              }}
+              <Typography
+                variant="h4"
+                style={{
+                  padding: "40px",
+                  borderLeft: "3px solid #A5B3C7",
+                  margin: "20px 0",
+                  color: "#013B41",
+                }}
+              >
+                Welcome to my projects section!
+              </Typography>
+            </motion.div>
+            <motion.div
+              ref={ref}
+              initial={{ opacity: 0, y: 50 }}
+              animate={inView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 1 }}
             >
-              Here, you can explore some of my latest web development projects
-              that showcase my skills and passion for creating engaging and
-              user-friendly websites. From responsive designs to intuitive user
-              interfaces, each project is a reflection of my dedication to
-              delivering high-quality solutions that meet the needs of my
-              clients and their users. So, take a look around and get inspired
-              by the possibilities of what we can create together.
-            </Typography>
-          </motion.div>
-        </InView>
-      </Box>
-      <Box
-        sx={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fit, minmax(400px, 2fr))",
-          gridAutoFlow: "row",
-          gap: "50px",
-          margin: "auto 0",
-          padding: "20px 0",
-        }}
-      >
-        {projects.map((project, index) => (
-          <div key={index} style={{ margin: "0 auto" }}>
-            <InView>
-              {({ ref, inView }) => (
-                <motion.div
-                  ref={ref}
-                  initial={{ opacity: 0, y: 50 }}
-                  animate={inView ? { opacity: 1, y: 0 } : {}}
-                  transition={{ duration: 1 }}
-                >
-                  <CardProject
-                    title={project.title}
-                    description={project.description}
-                    picBg={project.picBg}
-                    projectSummaryItems={project.summary}
-                    keyDetails={project.details}
-                    gitHubRef={project.git}
-                  />
-                </motion.div>
-              )}
-            </InView>
-          </div>
-        ))}
+              <Typography
+                style={{
+                  padding: "20px 0",
+                  margin: "20px 0",
+                  color: "#013B41",
+                  fontSize: "18px",
+                  lineHeight: "1.6",
+                  textAlign: "justify",
+                }}
+              >
+                Here, you can explore some of my latest web development projects
+                that showcase my skills and passion for creating engaging and
+                user-friendly websites. From responsive designs to intuitive
+                user interfaces, each project is a reflection of my dedication
+                to delivering high-quality solutions that meet the needs of my
+                clients and their users. So, take a look around and get inspired
+                by the possibilities of what we can create together.
+              </Typography>
+            </motion.div>
+          </InView>
+        </Box>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(400px, 2fr))",
+            gridAutoFlow: "row",
+            gap: "50px",
+            margin: "auto 0",
+            padding: "20px 0",
+          }}
+        >
+          {projects.map((project, index) => (
+            <div key={index} style={{ margin: "0 auto" }}>
+              <InView>
+                {({ ref, inView }) => (
+                  <motion.div
+                    ref={ref}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={inView ? { opacity: 1, y: 0 } : {}}
+                    transition={{ duration: 1 }}
+                  >
+                    <CardProject
+                      title={project.title}
+                      description={project.description}
+                      picBg={project.picBg}
+                      projectSummaryItems={project.summary}
+                      keyDetails={project.details}
+                      gitHubRef={project.git}
+                    />
+                  </motion.div>
+                )}
+              </InView>
+            </div>
+          ))}
+        </Box>
       </Box>
     </>
   );
