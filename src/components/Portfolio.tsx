1812ec29bfd786f6e0672ab7c1fd91f1557ff308
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-06-14 13:49:21 -0700

    Fixed meta tag component

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,29 +2,31 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import img from "../assets/sea.jpg";
 
-const Portfolio: React.FC = () => {
-  return (
-    <>
-      <Helmet>
-        <title>Nataliia Sokolova - Web Developer Portfolio</title>
-        <meta
-          name="description"
-          content="Welcome to my web developer portfolio!"
-        />
-        <meta property="og:title" content="Nataliia Sokolova's Portfolio" />
-        <meta
-          property="og:description"
-          content="Check out Nataliia Sokolova's web developer portfolio."
-        />
-        {/* <meta property="og:image" content="https://sokolova.ca/-og-image.jpg" /> */}
-        <meta property="og:image" content={img} />
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <>
+        <Helmet>
+          <title>Nataliia Sokolova - Web Developer Portfolio</title>
+          <meta
+            name="description"
+            content="Welcome to my web developer portfolio!"
+          />
+          <meta property="og:title" content="Nataliia Sokolova's Portfolio" />
+          <meta
+            property="og:description"
+            content="Check out Nataliia Sokolova's web developer portfolio."
+          />
+          {/* <meta property="og:image" content="https://sokolova.ca/-og-image.jpg" /> */}
+          <meta property="og:image" content={img} />
 
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-      </Helmet>
-    </>
-  );
-};
+          <meta property="og:image:type" content="image/jpeg" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+        </Helmet>
+      </>
+    );
+  }
+}
 
 export default Portfolio;
