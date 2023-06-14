032b8e0f099a996f630d131ec13d059e8c4bb51d
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-06-14 12:36:42 -0700

    Changed meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -40,20 +40,23 @@ function App() {
 
   return (
     <>
-      <Helmet>
-        <title>Nataliia Sokolova - Web Developer Portfolio</title>
-        <meta
-          name="description"
-          content="Welcome to my web developer portfolio!"
-        />
-        <meta property="og:title" content="Nataliia Sokolova's Portfolio" />
-        <meta
-          property="og:description"
-          content="Check out Nataliia Sokolova's web developer portfolio."
-        />
-        <meta property="og:image" content="https://sokolova.ca/-og-image.jpg" />
-      </Helmet>
       <ThemeProvider theme={defaultTheme}>
+        <Helmet>
+          <title>Nataliia Sokolova - Web Developer Portfolio</title>
+          <meta
+            name="description"
+            content="Welcome to my web developer portfolio!"
+          />
+          <meta property="og:title" content="Nataliia Sokolova's Portfolio" />
+          <meta
+            property="og:description"
+            content="Check out Nataliia Sokolova's web developer portfolio."
+          />
+          {/* <meta
+            property="og:image"
+            content="https://sokolova.ca/-og-image.jpg"
+          /> */}
+        </Helmet>
         <CssBaseline />
         <Header window={undefined} />
         <main>
